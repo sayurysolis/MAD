e45b303d625161e5b791fa186ed51c42e0507dfc
Raul <ragegaro@gmail.com>
2025-11-11 13:43:34 -0600

    queys olvidadoss

--- database/Gets.sql
+++ database/Gets.sql
@@ -21,7 +21,6 @@ GO
 
 
 
-
     CREATE VIEW Vista_Empleado AS
 SELECT 
     e.ID_Empleado,
@@ -51,3 +50,23 @@ INNER JOIN Departamento d ON e.DepID = d.ID_Departamento
 INNER JOIN Puesto p ON e.PuestoID = p.ID_Puesto;
 
 GO
+
+
+CREATE TABLE Matriz (
+    id_Matriz INT IDENTITY(1,1) PRIMARY KEY,
+    id_Empleado INT,
+    NombreEmpleado NVARCHAR(MAX),
+    Faltas INT,
+    Asistencia DECIMAL(10,2),
+    Puntualidad DECIMAL(10,2),
+    Despensa DECIMAL(10,2),
+    Vacaciones DECIMAL(10,2),
+    PrimaVacacional DECIMAL(10,2),
+    PrestamoEmpresa DECIMAL(10,2),
+    PrestamoInfo DECIMAL(10,2),
+    FondoAhorro DECIMAL(10,2),
+    Productividad DECIMAL(10,2),
+    HorasExtras INT,
+    PensionAlimenticia DECIMAL(10,2),
+    FOREIGN KEY (id_Empleado) REFERENCES Empleado(ID_Empleado)
+);
